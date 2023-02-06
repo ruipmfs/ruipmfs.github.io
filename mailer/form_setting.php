cab196cfb143c5a5f2c301d2ebd59d2f08280f52
Rui Santos <rui.pmfs@gmail.com>
2023-02-06 01:42:10 +0000

    Project Display Change

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from the site 'Rui Santos'"; //letter subject
-	$to = 'rui.pmfs@gmail.com'; // Enter Your E-mail
-	$from = 'admin@ruipmfs.com'; // Admin e-mail
-	$fromName = 'ruipmfs'; // Your company name
-	$charset = 'UTF-8';
-?>
